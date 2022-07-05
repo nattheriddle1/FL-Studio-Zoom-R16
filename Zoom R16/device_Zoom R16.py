575a2300213cc50b91c301553f58d7971f9b0dd6
nattheriddle1 <69092489+nattheriddle1@users.noreply.github.com>
2022-07-05 16:25:23 -0500

    Added function stubs

--- Zoom R16/device_Zoom R16.py
+++ Zoom R16/device_Zoom R16.py
@@ -1,1 +1,19 @@
 # name=Zoom R16
+
+
+def OnControlChange(event):
+    """
+    Handles the dial.
+    """
+
+
+def OnNoteOn(event):
+    """
+    Handles all keys.
+    """
+
+
+def OnPitchBend(event):
+    """
+    Handles all faders.
+    """
