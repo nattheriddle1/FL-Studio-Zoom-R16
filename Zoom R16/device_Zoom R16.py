759de6951db9f327e949745459721c448aab298b
nattheriddle1 <69092489+nattheriddle1@users.noreply.github.com>
2022-07-05 16:28:43 -0500

    Marked events as handled
    This prevents FL Studio from handling the events again after this script finishes handling them.

--- Zoom R16/device_Zoom R16.py
+++ Zoom R16/device_Zoom R16.py
@@ -5,15 +5,18 @@ def OnControlChange(event):
     """
     Handles the dial.
     """
+    event.handled = True
 
 
 def OnNoteOn(event):
     """
     Handles all keys.
     """
+    event.handled = True
 
 
 def OnPitchBend(event):
     """
     Handles all faders.
     """
+    event.handled = True
